d628b250e334f2ab167c7cf69a3a6926eb5b71c5
Your_Name <Your_Email>
2025-02-01 00:31:05 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-app/",
   plugins: [react()],
-  base: "/notice-board1"
+  base: "/notice-board/"
 })
